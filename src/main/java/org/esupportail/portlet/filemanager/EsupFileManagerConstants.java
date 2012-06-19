3f6d575503ae73cb15953d0ef5977804a59bf172
Vincent Bonamy <vbonamy@gmail.com>
2012-06-19 13:58:21 +0200

    EsupFileManagerConstants - fix some code ...

--- src/main/java/org/esupportail/portlet/filemanager/EsupFileManagerConstants.java
+++ src/main/java/org/esupportail/portlet/filemanager/EsupFileManagerConstants.java
@@ -8,11 +8,11 @@ import javax.xml.namespace.QName;
  */
 public final class EsupFileManagerConstants {
 
-    public static final String NAMESPACE = "https://source.jasig.org/schemas/uportal/download";
+    public static final String NAMESPACE = "https://www.esup-portail.org/schemas/esup-filemanager";
 
     public static final String DOWNLOAD_REQUEST_LOCAL_NAME = "DownloadRequest";
 
-    public static final QName download_REQUEST_QNAME = new QName(NAMESPACE, DOWNLOAD_REQUEST_LOCAL_NAME);
+    public static final QName DOWNLOAD_REQUEST_QNAME = new QName(NAMESPACE, DOWNLOAD_REQUEST_LOCAL_NAME);
 
     public static final String DOWNLOAD_REQUEST_QNAME_STRING = "{" + NAMESPACE + "}" + DOWNLOAD_REQUEST_LOCAL_NAME;
     
